7ab81b88cb87a3ad0d2a95da566307e184374a48
Ryan Hausmann <ryanhausmann3@gmail.com>
2025-12-22 15:26:40 -0500

    Add a few plugins to neovim

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- Vim options
+vim.g.mapleader = " "
+
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
@@ -19,5 +21,164 @@ vim.opt.updatetime = 250
 vim.opt.rtp:prepend("~/.local/share/nvim/lazy/lazy.nvim")
 
 require("lazy").setup({
+    -- Syntax highlighting
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        opts = {
+            ensure_installed = {
+                "c",
+                "cpp",
+                "rust",
+                "python",
+                "verilog",
+                "systemverilog",
+                "lua",
+            },
+            highlight = { enable = true },
+        },
+    },
 
+    -- Error highlighting / LSP
+    {
+        "williamboman/mason.nvim",
+        config = true,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = {
+                "clangd",
+                "rust_analyzer",
+                "pyright",
+                "svlangserver",
+                "lua_ls",
+            },
+        },
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.lsp.config("clangd", {})
+            vim.lsp.config("rust_analyzer", {})
+            vim.lsp.config("pyright", {})
+            vim.lsp.config("svlangserver", {})
+
+            vim.lsp.config("lua_ls", {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" }, -- to resolve 'Undefined global vim'
+                        },
+                    },
+                },
+            })
+        end,
+    },
+
+    -- Autocomplete
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+
+                -- Setup so tab = go down, shift+tab = go up, enter = select
+                mapping = {
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif require("luasnip").expand_or_jumpable() then
+                            require("luasnip").expand_or_jump()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s"}),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    ["<CR>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() and cmp.get_selected_entry() then
+                            cmp.confirm({ select = false })
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                },
+
+                sources = {
+                    { name = "nvim_lsp" },
+                },
+            })
+        end,
+    },
+
+    -- File tree
+    {
+        "nvim-tree/nvim-tree.lua",
+        config = true,
+    },
+
+    -- Fuzzy finder
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+})
+
+-- Auto show errors when hovering over
+vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+        vim.diagnostic.open_float(nil, {
+            focus = false,
+            scope = "cursor",
+        })
+    end,
+})
+
+-- Configure autocomplete to not automatically select anything
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+-- Configure nvim-tree
+require("nvim-tree").setup({
+    hijack_cursor = true,
+    update_cwd = true,
+    view = {
+        width = 30,
+        side = "left",
+    },
+})
+
+-- Open tree on nvim startup
+vim.api.nvim_create_autocmd("VimEnter", {
+    callback = function()
+        require("nvim-tree.api").tree.open()
+        vim.cmd("wincmd p") -- Move cursor back to buffer, not tree
+    end
+})
+
+-- Close tree on last buffer close
+vim.api.nvim_create_autocmd("BufEnter", {
+    nested = true,
+    callback = function()
+        if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == "NvimTree" then
+            vim.cmd "quit"
+        end
+    end
 })
