25e99386e6e3360365cfb7bb96b22947a87179b0
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-01-12 11:11:24 -0500

    Manually install clangd for Mason on ARM systems, since it doesn't work for some reason

--- .config/yadm/packages.sh
+++ .config/yadm/packages.sh
@@ -33,4 +33,18 @@ detect_package_manager() {
 install_packages() {
     detect_package_manager
     eval "$PKG_MGR_CMD ${PACKAGES[*]}"
+
+    # Fix for ARM systems to manually install clangd for Mason
+    ARCH=$(uname -m)
+    if [[ "$ARCH" == "aarch64" ]]; then
+        # From https://github.com/mason-org/mason.nvim/issues/1578#issuecomment-2455253723
+        eval "$PKG_MGR_CMD mason clangd jq"
+        mkdir -p ~/.local/share/nvim/mason/packages/clangd/mason-schemas
+        cd ~/.local/share/nvim/mason/packages/clangd
+        curl https://raw.githubusercontent.com/clangd/vscode-clangd/master/package.json \
+            | jq .contributes.configuration > mason-schemas/lsp.json
+        echo '{"schema_version":"1.1","primary_source":{"type":"local"},"name":"clangd","links":{"share":{"mason-schemas/lsp/clangd.json":"mason-schemas/lsp.json"}}}' \
+            > mason-receipt.json
+        cd -
+    fi
 }
