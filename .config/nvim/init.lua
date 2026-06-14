30e77783668b5e3f9a9fe820810ed32d52bf3fc1
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-06-14 19:39:02 -0400

    Disable zellij-nav.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -288,18 +288,18 @@ require("lazy").setup({
     -- },
 
     -- Integration with zellij and better window navigation
-    {
-        "swaits/zellij-nav.nvim",
-        lazy = true,
-        event = "VeryLazy",
-        keys = {
-            { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",    { silent = true, desc = "navigate left or tab"  } },
-            { "<c-j>", "<cmd>ZellijNavigateDown<cr>",       { silent = true, desc = "navigate down"         } },
-            { "<c-k>", "<cmd>ZellijNavigateUp<cr>",         { silent = true, desc = "navigate up"           } },
-            { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>",   { silent = true, desc = "navigate right or tab" } },
-        },
-        opts = {},
-    },
+    -- {
+    --     "swaits/zellij-nav.nvim",
+    --     lazy = true,
+    --     event = "VeryLazy",
+    --     keys = {
+    --         { "<c-h>", "<cmd>ZellijNavigateLeftTab<cr>",    { silent = true, desc = "navigate left or tab"  } },
+    --         { "<c-j>", "<cmd>ZellijNavigateDown<cr>",       { silent = true, desc = "navigate down"         } },
+    --         { "<c-k>", "<cmd>ZellijNavigateUp<cr>",         { silent = true, desc = "navigate up"           } },
+    --         { "<c-l>", "<cmd>ZellijNavigateRightTab<cr>",   { silent = true, desc = "navigate right or tab" } },
+    --     },
+    --     opts = {},
+    -- },
 
     -- -- Github themes
     -- {
