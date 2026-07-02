b84aace8a0a2e366809c2bba2db23716347481dd
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-07-02 11:46:03 -0400

    Add script that installs niri & other related packages

--- .config/yadm/install_niri.py
+++ .config/yadm/install_niri.py
@@ -1,2 +1,9 @@
+from utils import install_package
 def install_niri():
+    # for now, fedora only
     print("Installing Niri and related items...")
+
+    install_package("niri")      # window manager
+    install_package("alacritty") # terminal emulator
+    install_package("hypridle")  # idle manager
+    install_package("hyprlock")  # lock screen
