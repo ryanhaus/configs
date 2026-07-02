bdd743617a8ee0989a443ea397b288257d024504
Ryan Hausmann <ryanhausmann3@gmail.com>
2026-07-02 11:41:02 -0400

    Update script that installs fish/shell utilities

--- .config/yadm/install_fish.py
+++ .config/yadm/install_fish.py
@@ -1,2 +1,13 @@
+from utils import install_package
+import subprocess
+
 def install_fish():
+    # right now, fedora only, since there is no apt package for some things here
     print("Installing fish and related items...")
+
+    subprocess.run(["sudo", "dnf", "copr", "enable", "-y", "claaj/typst"])
+
+    install_package("fish")   # shell
+    install_package("zellij") # terminal multiplexing
+    install_package("zoxide") # smarter 'cd' command
+    install_package("typst")  # for writing (typesetter)
